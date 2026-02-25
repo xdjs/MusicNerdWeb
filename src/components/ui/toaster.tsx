7920e0c3fee1a5ffa993bd9a7507f3555fad9bac
Carl Tydingco <clt@xdjs.com>
2026-02-25 10:58:29 -0800

    Revert "fix: defer logout toast to after reload and set 3s global toast duration"
    This reverts commit 1caf1c72ce0c68436d9371f24c28fda9d9c685f8.

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -14,7 +14,7 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider duration={3000}>
+    <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
